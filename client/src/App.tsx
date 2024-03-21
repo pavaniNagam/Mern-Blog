8a2dc80f435deb58f42eaed064e5a846db8627c7
Pavani Nagam <pavani_nagam@Pavanis-MacBook-Pro.local>
2024-03-21 22:12:16 +0530

    adding pages and routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Signin from './pages/Signin';
+import Signup from './pages/Signup';
+import Dashboard from './pages/Dashboard';
+import Project from './pages/Project';
 
 export default function App() {
   return (
-    <h1 className='text-3l text-red-500'>App test</h1>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/sign-in' element={<Signin />} />
+        <Route path='/sign-up' element={<Signup />} />
+        <Route path='/dashboard' element={<Dashboard />} />
+        <Route path='/projects' element={<Project />} />
+      </Routes>
+    </BrowserRouter>)
 }
+
